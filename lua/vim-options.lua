cc4c04ad65757670d2000994e98447b181db4cbc
phortheman <marcuspullo@gmail.com>
2024-03-18 22:23:15 -0400

    Initial commit

---
+++ lua/vim-options.lua
@@ -0,0 +1,8 @@
+
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=4")
+vim.cmd("set softtabstop=4")
+vim.cmd("set shiftwidth=4")
+vim.cmd("set list")
+vim.cmd("set listchars=space:·,tab:>-,trail:~,extends:>,precedes:<")
+vim.g.mapleader = " "
