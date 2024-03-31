976764e02638a59789e5a7beb8277fdb846a8d49
phortheman <marcuspullo@gmail.com>
2024-03-30 20:09:12 -0400

    Setup autoformat on write
    Setup an autocmd to run the vim.lsp.buf.format function when writing the
    buffer

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -8,6 +8,11 @@ return {
 				null_ls.builtins.formatting.goimports,
 			},
 		})
+
+		-- Shortcut to format the current buffer
 		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+
+		-- Auto format on buffer write
+		vim.cmd([[autocmd BufWritePre * lua vim.lsp.buf.format()]])
 	end,
 }
