bd435eb2653445ca73f3dc1a685f3daaab1de1ea
phortheman <marcuspullo@gmail.com>
2024-03-30 20:06:51 -0400

    Added silent ot gitstuff.lua

--- lua/plugins/gitstuff.lua
+++ lua/plugins/gitstuff.lua
@@ -6,8 +6,8 @@ return {
 		"lewis6991/gitsigns.nvim",
 		config = function()
 			require("gitsigns").setup()
-			vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
-			vim.keymap.set("n", "<leader>gt", ":Gitsigns toggle_current_line_blame<CR>", {})
+			vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", { silent = true })
+			vim.keymap.set("n", "<leader>gt", ":Gitsigns toggle_current_line_blame<CR>", { silent = true })
 		end,
 	},
 }
