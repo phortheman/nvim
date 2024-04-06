477f558e566674dd268474217fe4abda5b09e657
phortheman <marcuspullo@gmail.com>
2024-04-06 16:27:56 -0400

    Added autotag and incremental selection for Treesitter
    - Added autotag plugin if I find myself writting HTML
    - Added keymaps for incremental selection

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,6 +1,9 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
+	dependencies = {
+		"windwp/nvim-ts-autotag",
+	},
 	config = function()
 		local config = require("nvim-treesitter.configs")
 		config.setup({
@@ -8,6 +11,16 @@ return {
 			sync_install = false,
 			highlight = { enable = true },
 			indent = { enable = true },
+			autotag = { enable = true },
+			incremental_selection = {
+				enable = true,
+				keymaps = {
+					init_selection = "<C-space>",
+					node_incremental = "<C-space>",
+					scope_incremental = false,
+					node_decremental = "<bs>",
+				},
+			},
 		})
 	end,
 }
