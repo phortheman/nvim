cd5f62fe7bf61983736e8c664e557194280152b0
phortheman <marcuspullo@gmail.com>
2024-04-06 16:28:21 -0400

    Added new dependency for nvim-dap-ui

--- lua/plugins/debugging.lua
+++ lua/plugins/debugging.lua
@@ -4,6 +4,7 @@ return {
 		dependencies = {
 			"rcarriga/nvim-dap-ui",
 			"leoluz/nvim-dap-go",
+			"nvim-neotest/nvim-nio",
 		},
 		config = function()
 			local dap, dapui = require("dap"), require("dapui")
