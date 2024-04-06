6002d407b90349220fb15cbfc1215feb3c944808
phortheman <marcuspullo@gmail.com>
2024-04-06 17:01:32 -0400

    Added ability for LSPs load with default capabilities
    - No longer need to do lspconfig.<LSP>.setup for every single one added
    - Still need to explore how to do it with formatters

--- lua/plugins/lsp/lsp-config.lua
+++ lua/plugins/lsp/lsp-config.lua
@@ -5,15 +5,20 @@ return {
 		"hrsh7th/cmp-nvim-lsp",
 	},
 	config = function()
+		local lspconfig = require("lspconfig")
+		local mason_lspconfig = require("mason-lspconfig")
 		local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-		local lspconfig = require("lspconfig")
-		lspconfig.lua_ls.setup({
-			capabilities = capabilities,
+		-- Automatically setup servers using the default capabilities
+		mason_lspconfig.setup_handlers({
+			function(server_name)
+				lspconfig[server_name].setup({
+					capabilities = capabilities,
+				})
+			end,
+			-- Add custom settings required below
 		})
-		lspconfig.gopls.setup({
-			capabilities = capabilities,
-		})
+
 		vim.keymap.set("n", "<leader>gD", vim.lsp.buf.declaration, { desc = "Show declaration" })
 		vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, { desc = "Go to definition" })
 		vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Show hover" })
