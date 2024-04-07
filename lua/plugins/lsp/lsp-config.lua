5e0ce3acad834a88229202b2afda007d702544b1
phortheman <marcuspullo@gmail.com>
2024-04-07 17:13:56 -0400

    Changed lsp-config to use Telescope UI for functions that support it

--- lua/plugins/lsp/lsp-config.lua
+++ lua/plugins/lsp/lsp-config.lua
@@ -19,12 +19,38 @@ return {
 			-- Add custom settings required below
 		})
 
-		vim.keymap.set("n", "<leader>gD", vim.lsp.buf.declaration, { desc = "Show declaration" })
-		vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, { desc = "Go to definition" })
-		vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Show hover" })
-		vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, { desc = "Code action" })
-		vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, { desc = "Find references" })
-		vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { desc = "Rename symbol" })
+		-- LSP functions that can use Telescope UI
+		vim.keymap.set("n", "<leader>gd", require("telescope.builtin").lsp_definitions, { desc = "Go to definition" })
+		vim.keymap.set("n", "<leader>gr", require("telescope.builtin").lsp_references, { desc = "Find references" })
+		vim.keymap.set(
+			"n",
+			"<leader>gI",
+			require("telescope.builtin").lsp_implementations,
+			{ desc = "[G]oto [I]mplementation" }
+		)
+		vim.keymap.set(
+			"n",
+			"<leader>D",
+			require("telescope.builtin").lsp_type_definitions,
+			{ desc = "Type [D]efinition" }
+		)
+		vim.keymap.set(
+			"n",
+			"<leader>ds",
+			require("telescope.builtin").lsp_document_symbols,
+			{ desc = "[D]ocument [S]ymbols" }
+		)
+		vim.keymap.set(
+			"n",
+			"<leader>ws",
+			require("telescope.builtin").lsp_dynamic_workspace_symbols,
+			{ desc = "[W]orkspace [S]ymbols" }
+		)
+
+		vim.keymap.set("n", "<leader>gD", vim.lsp.buf.declaration, { desc = "[G]oto [D]eclaration" })
+		vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Documentation Hover" })
+		vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, { desc = "[C]ode [A]ction" })
+		vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { desc = "[R]e[n]ame symbol" })
 		vim.keymap.set("n", "<leader>ds", vim.lsp.buf.document_symbol, { desc = "Document symbols" })
 		vim.keymap.set("n", "<leader>dl", vim.diagnostic.setloclist, { desc = "Open diagnostic list" })
 	end,
