0ca43770dd937bd4c5554905e15d67ad36364c53
phortheman <marcuspullo@gmail.com>
2024-04-07 17:13:21 -0400

    Formatting Changes
    - Moved everything out of the config function and into opts

--- lua/plugins/lsp/formatting.lua
+++ lua/plugins/lsp/formatting.lua
@@ -1,29 +1,31 @@
 return {
 	"stevearc/conform.nvim",
 	event = { "BufReadPre", "BufNewFile" },
-	config = function()
-		local conform = require("conform")
-
-		conform.setup({
-			-- Map of filetype to formatters
-			formatters_by_ft = {
-				lua = { "stylua" },
-				go = { "goimports", "gofmt" },
-			},
-			format_on_save = {
-				lsp_fallback = true,
-				timeout_ms = 500,
-			},
-			-- Conform will notify you when a formatter errors
-			notify_on_error = true,
-			-- Custom formatters and changes to built-in formatters
-		})
-
-		vim.keymap.set({ "n", "v" }, "<leader>gf", function()
-			conform.format({
-				lsp_fallback = true,
-				timeout_ms = 1000,
-			})
-		end, { desc = "Format the file or range in visual mode" })
-	end,
+	keys = {
+		{
+			"<leader>f",
+			function()
+				require("conform").format({
+					lsp_fallback = true,
+					timeout_ms = 1000,
+					async = true,
+				})
+			end,
+			mode = "",
+			desc = "[F]ormat the file or range in visual mode",
+		},
+	},
+	opts = {
+		formatters_by_ft = {
+			lua = { "stylua" },
+			go = { "goimports", "gofmt" },
+		},
+		format_on_save = {
+			lsp_fallback = true,
+			timeout_ms = 500,
+		},
+		-- Conform will notify you when a formatter errors
+		notify_on_error = true,
+		-- Custom formatters and changes to built-in formatters
+	},
 }
