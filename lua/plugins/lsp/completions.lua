e6d85e7d24e929db411e345f536d5fab8657fea5
phortheman <marcuspullo@gmail.com>
2024-04-07 12:47:07 -0400

    Changed autocomplete keymaps
    - After watchign a video I learned that C-p and C-n are vim binding for
    these kind of menus
    - Change confirm to C-y because I have been annoyed with hitting enter
    after ignoring a autocomplete and it putting it in anyways

--- lua/plugins/lsp/completions.lua
+++ lua/plugins/lsp/completions.lua
@@ -26,13 +26,13 @@ return {
 					documentation = cmp.config.window.bordered(),
 				},
 				mapping = cmp.mapping.preset.insert({
-					["<C-k>"] = cmp.mapping.select_prev_item(),
-					["<C-j>"] = cmp.mapping.select_next_item(),
-					["<C-n>"] = cmp.mapping.scroll_docs(-4),
-					["<C-f>"] = cmp.mapping.scroll_docs(4),
+					["<C-p>"] = cmp.mapping.select_prev_item(),
+					["<C-n>"] = cmp.mapping.select_next_item(),
+					["<C-P>"] = cmp.mapping.scroll_docs(-4),
+					["<C-N>"] = cmp.mapping.scroll_docs(4),
 					["<C-Space>"] = cmp.mapping.complete(),
 					["<C-e>"] = cmp.mapping.abort(),
-					["<CR>"] = cmp.mapping.confirm({ select = true }),
+					["<C-y>"] = cmp.mapping.confirm({ select = true }),
 				}),
 				sources = cmp.config.sources({
 					{ name = "nvim_lsp" },
