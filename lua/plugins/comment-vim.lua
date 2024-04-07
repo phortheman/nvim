4242f31e44715f3157d1538e7c618b2a97596b9c
phortheman <marcuspullo@gmail.com>
2024-04-07 17:42:40 -0400

    Added todo-comments

--- lua/plugins/comment-vim.lua
+++ lua/plugins/comment-vim.lua
@@ -1,26 +1,34 @@
 return {
-	-- "tpope/vim-commentary", cool but uses vimscript and unsure how to customize the mappings
-	"numToStr/Comment.nvim",
-	event = { "BufReadPre", "BufNewFile" },
-	config = function()
-		require("Comment").setup({
-			toggler = {
-				line = "<leader>gcc",
-				block = "<leader>gbc",
-			},
-			opleader = {
-				line = "<leader>gcc",
-				block = "<leader>gbc",
-			},
-			extra = {
-				above = "<leader>gcO",
-				below = "<leader>gco",
-				eol = "<leader>gcA",
-			},
-			mappings = {
-				basic = false,
-				extra = false,
-			},
-		})
-	end,
+	{
+		-- "tpope/vim-commentary", cool but uses vimscript and unsure how to customize the mappings
+		"numToStr/Comment.nvim",
+		event = { "BufReadPre", "BufNewFile" },
+		config = function()
+			require("Comment").setup({
+				toggler = {
+					line = "<leader>gcc",
+					block = "<leader>gbc",
+				},
+				opleader = {
+					line = "<leader>gcc",
+					block = "<leader>gbc",
+				},
+				extra = {
+					above = "<leader>gcO",
+					below = "<leader>gco",
+					eol = "<leader>gcA",
+				},
+				mappings = {
+					basic = false,
+					extra = false,
+				},
+			})
+		end,
+	},
+	{
+		"folke/todo-comments.nvim",
+		event = "VimEnter",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		opts = { signs = false },
+	},
 }
