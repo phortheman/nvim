c78ca5caa1d0508531a08c24b0f02e3189efd08e
phortheman <marcuspullo@gmail.com>
2024-05-12 23:53:03 -0400

    feat: added purposed new vim keymap for moving a line/highlighted up and down

--- lua/vim-remaps.lua
+++ lua/vim-remaps.lua
@@ -34,3 +34,7 @@ vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left wind
 vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
 vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
 vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
+
+-- I want these. Need to figure out a keymap
+-- vim.keymap.set("v", "K", ":m '>+1<CR>gv=gv", { desc = "Move highlighted up" })
+-- vim.keymap.set("v", "J", ":m '>-2<CR>gv=gv", { desc = "Move highlighted down" })
