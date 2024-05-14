791cb319ff14407d252dcafb4f157cf60385a92e
phortheman <marcuspullo@gmail.com>
2024-05-14 09:47:14 -0400

    feat: added vimdadbod and vimdadbodui
    - doesn't work well with remote databases but could be helpful if using a sqllite database

--- lua/plugins/lsp/completions.lua
+++ lua/plugins/lsp/completions.lua
@@ -64,5 +64,13 @@ return {
 				{ name = "buffer" },
 			}),
 		})
+
+		-- Setup for Vim-DadBod
+		cmp.setup.filetype({ "sql", "plsql", "mysql" }, {
+			sources = {
+				{ name = "vim-dadbod-completion" },
+				{ name = "buffer" },
+			},
+		})
 	end,
 }
