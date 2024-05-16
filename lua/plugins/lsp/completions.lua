494cb2f6fe1aeabfbb7317642bb698f3e3bea222
phortheman <marcuspullo@gmail.com>
2024-05-16 15:00:41 -0400

    feat: added jumping to luasnip

--- lua/plugins/lsp/completions.lua
+++ lua/plugins/lsp/completions.lua
@@ -65,5 +65,16 @@ return {
 			}),
 		})
 
+		vim.keymap.set({ "i", "s" }, "<C-k>", function()
+			if luasnip.expand_or_jumpable() then
+				luasnip.expand_or_jump()
+			end
+		end, { silent = true })
+
+		vim.keymap.set({ "i", "s" }, "<C-j>", function()
+			if luasnip.expand_or_jumpable(-1) then
+				luasnip.expand_or_jump(-1)
+			end
+		end, { silent = true })
 	end,
 }
