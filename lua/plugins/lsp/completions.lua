6ad6e8ceeb594b7366ac4eff538df51d58a61a73
phortheman <marcuspullo@gmail.com>
2024-05-16 14:57:18 -0400

    fix: added buffer to cmp source correctly

--- lua/plugins/lsp/completions.lua
+++ lua/plugins/lsp/completions.lua
@@ -60,7 +60,7 @@ return {
 			sources = cmp.config.sources({
 				{ name = "nvim_lsp" },
 				{ name = "luasnip" }, -- For luasnip users.
-			}, {
+				{ name = "path" },
 				{ name = "buffer" },
 			}),
 		})
